6e32cc08b808d984b0d4edc9477262e45c332b16
JoseRodriguez <angel102030muro@gmail.com>
2026-02-08 11:13:43 -0800

    Added binary search function but has errors because comapreCase is missing.

--- Main.cpp
+++ Main.cpp
@@ -196,7 +196,23 @@ void Inventory::addProduct() {
 }
 
 int Inventory::searchProduct(const string& targetProduct) {
+    int low = 0;
+    int high = count - 1;
 
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (product[mid].productName == targetProduct) {
+            return mid;
+        }
+        else if (product[mid].productName < targetProduct) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1;
 }
 
 void Inventory::selectOption() {
